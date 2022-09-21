3aacf26bebb347ee026bf01fbcd204a454bfede1
yyhome-tromb <107352731+yyhome-tromb@users.noreply.github.com>
2022-09-22 02:21:07 +0900

    fix DOM bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const gas = google.script;
-const ul = document.getElementsByName("ul");
+const ul = document.getElementsByName("ul")[0];
 
 window.addEventListener("DOMContentLoaded", () => {
   gas.run.withSuccessHandler(show_list).load_drive();
